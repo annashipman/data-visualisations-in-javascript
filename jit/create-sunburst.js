fbefd8e1cbb3be05bc1bf4291bbfe5358cbb251b
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-07-01 23:17:04 +0100

    adding on click and hover over events

--- jit/create-sunburst.js
+++ jit/create-sunburst.js
@@ -1,12 +1,55 @@
 function init(){
-	
- 	var sb = new $jit.Sunburst({
+
+   var sb = new $jit.Sunburst({
         injectInto: 'infovis',
         levelDistance: 90,
-        Node: { overridable: true } 
+        Node: { overridable: true },
+        Label: { type: "Native" },
+        Events: {
+          enable: true,
+          onClick: function(node) {
+            if(!node) return;
+            var caption = node.name + "\n", data = node.data;
+            if("type" in data) {
+              caption += "Type: " + data.type+"\n";
+            }
+            if("startDate" in data) {
+              caption += "Work started on: " + data.startDate + "\n";
+            }
+            if("endDate" in data) {
+              caption += "In production on: " + data.endDate+"\n";
+            }
+            if("description" in data) {
+              caption += data.description;
+            }
+
+            $jit.id('inner-details').innerHTML = caption;
+          }
+        },
+        Tips: {
+          enable: true,
+          onShow: function(tip, node) {
+            var html = "<div class=\"tip-title\">" + node.name + "</div>"; 
+            var data = node.data;
+            if("timeTaken" in data) {
+              html += "<b>Time Taken:</b> " + data.timeTaken + " days";
+            }
+            tip.innerHTML = html;
+          }
+        },
+        NodeStyles: {
+          enable: true,
+          type: 'Native',
+          stylesClick: {
+            'color': "#36305D"
+          },
+          stylesHover: {
+            'color': "#3E56A1"
+          }
+        } 
     });
 
     sb.loadJSON(json);
     sb.refresh(); 
-
+    
 }
