1d94be6f739a4ea81b95b88ba62492ad3afb550f
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-07-02 02:21:08 +0100

    oops, an extra semi-colon where none was required

--- raphael/types-by-month.js
+++ raphael/types-by-month.js
@@ -2,7 +2,7 @@ function init() {
 
 	var r = Raphael("holder", 600, 800);
 
-	var data = [10,10,25,50];
+	var data = [10,10,25,50],
 		colours = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"];	
     
     r.piechart(125, 125, 100, data, {colors:colours});
