ada16707904e07f1ab9657a79cff9027e7ec459c
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-07-02 13:22:43 +0100

    merging line and hover

--- raphael/types-by-month.js
+++ raphael/types-by-month.js
@@ -1,33 +1,42 @@
 function init() {
 
-	var r = Raphael("holder", 600, 800);
-
-	var sortedData = sort_data(),
-		colours = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"];	
-    
+    var r = Raphael("holder", 2000, 800),
+        colours = ["#225533", "#44bbcc", "#88dddd", "#bbeeff"],
+        sortedData = sort_data(),
+        flag;
+   
     var x = 50,
-       	line = 1,
-       	radius = 25,
-       	labelOffset = radius + 10,
-       	pieSpacing = radius + 55,
-       	y = pieSpacing*line;
+        line = 1,
+        radius = 25,
+        labelOffset = radius + 10,
+        pieSpacing = radius + 55,
+        y = pieSpacing*line;
+
+    var lineX = [],
+      lineYFeatures = [],
+      lineYBugs = [],
+      lineYInfrastructure = [],
+      lineYMaintenance = [];
 
     sortedData.forEach(function(month) {
 
-    	var data = [month.features, 
-    		month.bugs, 
-    		month.infrastructure, 
-    		month.maintenance];
-    	
-      	var pie = r.piechart(x, y, radius, data, {colors:colours})
-      		.hover(
-            	function() { 
-            		flag = r.flag(this.mx, this.my, this.value.value) 
-            	},
-                function(){ 
-            		flag.remove()
-            });	
+      var data = [month.features, month.bugs, month.infrastructure, month.maintenance];
+      
+        var pie = r.piechart(x, y, radius, data, {colors:colours})
+        .hover(
+            function() { 
+              flag = r.flag(this.mx, this.my, this.value.value) 
+            },
+            function(){ 
+              flag.remove()
+          });  
             
+        lineX.push(sortedData.indexOf(month));
+        lineYFeatures.push(month.features);
+        lineYBugs.push(month.bugs);
+        lineYInfrastructure.push(month.infrastructure);
+        lineYMaintenance.push(month.maintenance); 
+
         r.text(x, y + labelOffset, month.monthString);
         x += pieSpacing;
 
@@ -35,18 +44,29 @@ function init() {
         if (monthNumber % 6 == 0) {
             x = 50;
             line++;
-		    y = pieSpacing * line;
-	    }
+        y = pieSpacing * line;
+      }
+
     });
     
     x += pieSpacing;
     line++;
     y = pieSpacing * line;
-	
-	var legendOptions = { 
-		legend: ["Features", "Bugs", "Infrastructure", "Maintenance"], 
-		legendpos: "east",  
-		colors: colours}
-	
-	pie = r.piechart(x, y, radius, [1, 1, 1, 1], legendOptions);
+  
+  var legendOptions = { 
+    legend: ["Features", "Bugs", "Infrastructure", "Maintenance"], 
+    legendpos: "east",  
+    colors: colours}
+  
+  pie = r.piechart(x, y, radius, [1, 1, 1, 1], legendOptions);
+
+  var lineData = [lineYFeatures, lineYBugs, lineYInfrastructure, lineYMaintenance];
+  
+  r.linechart(700, 60, 300, 220, lineX, lineData, { 
+    axis: "0 0 1 1", 
+    axisxstep: 11, 
+    smooth: true, 
+    colors: colours 
+   })
+
 }
